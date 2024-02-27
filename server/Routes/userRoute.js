d3ad239816ced6093e8a2528c63fd56d8e2f5504
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2024-02-27 04:21:26 -0800

    findUser and getAllUsers apis finished

--- server/Routes/userRoute.js
+++ server/Routes/userRoute.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../Controllers/userController");
+const {
+  registerUser,
+  loginUser,
+  findUser,
+  getAllUsers,
+} = require("../Controllers/userController");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/:userId", findUser);
+router.get("/", getAllUsers);
 
 module.exports = router;
