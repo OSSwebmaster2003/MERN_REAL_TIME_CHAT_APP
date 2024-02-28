173693734eb3d727724ad15ef3d34139aca5b9b0
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2024-02-28 05:30:46 -0800

    messages apis finished

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./Routes/userRoute");
 const chatRoute = require("./Routes/chatRoute");
+const messageRoute = require("./Routes/messageRoute");
 
 const app = express();
 dotenv.config();
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/users", userRoute);
 app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
 
 app.get("/", (req, res) => {
   res.send("Welcome to our chat app APIs...");
